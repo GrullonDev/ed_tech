d8f202a9c9be05970b59491ec04c188b24303f0e
Jorge-Tropigas <jorge.grullon@tomzagroup.com>
2026-09-04 16:16:30 -0600

    feat(app): inicializa Hive antes de runApp
    - main async con WidgetsFlutterBinding.ensureInitialized y LocalStorageService.init()

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'package:edtech_tiktok/app.dart';
+import 'package:edtech_tiktok/core/service/local_storage_service.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await LocalStorageService.init();
   runApp(const MyApp());
 }
